72b9a828d22b5325d1489cc87b06a202be0a1297
b007822 <b007822@C1710681>
2022-10-27 12:36:28 +0200

    Add project files.

---
+++ EventService/Controllers/EventController.cs
@@ -0,0 +1,76 @@
+﻿using EventService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using static EventService.Models.Event;
+
+namespace EventService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private static List<Event>? Events = new(); 
+ 
+        /// <summary>
+        /// Kaldes af en anden service, når den har brug for at offentliggøre (publishe) et event
+        /// </summary>
+        /// <param name="e">Information om den event, der er opstået</param>
+        [HttpPost]
+        public void RaiseEvent(Event e)
+        {            
+            e.EventNo = e.NextEventNo(Events);
+            Events?.Add(e);
+        }
+
+        /// <summary>
+        /// Henter events
+        /// </summary>
+        /// <param name="startIndex">Index på det første event der skal hentes</param>
+        /// <param name="antal">Antallet af events der maksimalt skal hentes (der kan være færre)</param>
+        /// <returns></returns>
+        [HttpGet]
+        public List<Event> ListEvents (int startIndex, int antal)
+        {
+
+            //Dan testdata
+            Event e = new();
+            e.TestData(Events);
+
+            //Tjek om parametre har en værdi
+            if (startIndex == null) 
+            {
+                startIndex = 0;
+            }
+            if (antal == null)
+            {
+                antal = Events.Count;
+            }
+
+            // Jeg ville egentlig helst have brugt eventId, da det er det "officielle" nummer på eventen. Men dewr er noget galt med  min tildeling med funktionen NextEventNo().
+            // Så det var lidt lettere blot at bruge listens indeks.
+
+            //Tjek om startindex er inden for range.
+            if (startIndex < 0 || startIndex >= Events.Count) 
+            {
+                startIndex = 0; 
+            }
+            // Tjek om startindex + antal er inden for range. Hvis ikke, så medtag hele range.
+            if (startIndex + antal > Events.Count)
+            {
+                
+                antal = Events.Count - startIndex;
+            }
+
+            // Læg range ind i ny liste og returner den
+            List<Event> EventsSubset = Events.GetRange(startIndex, antal);
+            return EventsSubset;
+        }
+    
+
+
+    }
+}
---
+++ EventService/Models/Event.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace EventService.Models
+{
+    /// <summary>
+    /// Information om den event, der er opstået
+    /// Tænk over, hvilke data det er relevant at event'et indeholder
+    /// </summary>
+    public class Event
+    {
+
+                public int EventNo { get; set; }        
+        public int TableNo { get; set; }
+        public int PizzaNo { get; set; }
+        
+        
+       //Genereer testdata, så listen er populeret uanset om der POST'es først.
+        public void TestData(List<Event>? Elist)
+        {
+
+            for (int i = 1; i < 4; i++)
+            {
+                Event e = new();
+                e.EventNo = NextEventNo(Elist);              
+                e.TableNo = i;
+                e.PizzaNo = i * 10;
+
+                Elist.Add(e);
+
+            }
+
+        }
+
+        //Tildeler det næste Eventnummer - virker ikke
+        public int NextEventNo(List<Event>? Elist)
+        {
+            if ((Elist != null) && (!Elist.Any()))
+            {
+                return Elist.Count + 1;
+            }
+            else
+                return 1;
+
+        }
+    }
+}
